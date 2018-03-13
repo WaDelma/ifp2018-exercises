0718ef940373b17cf31fb5ba1aa8527712790d9d
WaDelma <delma@del.ma>
2018-03-13 19:28:06 +0200

    Untested start.

--- W1.hs
+++ W1.hs
@@ -9,24 +9,34 @@ module W1 where
 -- Ex 1: define variables one, two and three. They should all have
 -- type Int and values 1, 2 and 3. This exercise has no tests.
 
+one :: Int
+one = 1
+
+two :: Int
+two = 2
+
+three :: Int
+three = 3
+
 -- Ex 2: define the function double of type Integer->Integer. Double
 -- should take one argument and return it multiplied by two.
 
 double :: Integer -> Integer
-double x = undefined
+double x = x + x
 
 -- Ex 3: define the function quadruple that uses the function double
 -- from the previous exercise to return its argument multiplied by
 -- four.
 
 quadruple :: Integer -> Integer
-quadruple x = undefined
+quadruple = double double
 
 -- Ex 4: define the function poly2. It should take four arguments of
 -- type Double, a, b, c, and x and return a*x^2+b*x+c. Give poly2 a
 -- type signature, i.e. poly2 :: something.
 
-poly2 = undefined
+poly2 :: Double -> Double -> Double -> Double -> _
+poly2 a b c x = a*x^2 + b*x + c
 
 -- Ex 5: define the function eeny that returns "eeny" for even inputs
 -- and "meeny" for odd inputs.
@@ -34,7 +44,8 @@ poly2 = undefined
 -- Ps. have a look at the built in function "even"
 
 eeny :: Integer -> String
-eeny = undefined
+eeny n if even n = "eeny"
+eeny _ = "meeny"
 
 -- Ex 6: fizzbuzz! Define the a function fizzbuzz that returns "Fizz"
 -- for numbers divisible by 3, "Buzz" for numbers divisible by 5, and
@@ -43,7 +54,11 @@ eeny = undefined
 --
 -- You can use the function mod to compute modulo.
 
-fizzbuzz = undefined
+fizzbuzz n = f (n (mod) 3) (n (mod) 5)
+    where f 0 0 = "FizzBuzz"
+          f 0 _ = "Buzz"
+          f _ 0 = "Fizz"
+          f _ _ = ""
 
 -- Ex 7: define a function isZero that returns True if it is given an
 -- Integer that is 0, and False otherwise. Give isZero a type signature.
@@ -52,20 +67,23 @@ fizzbuzz = undefined
 --
 -- Ps. the type of booleans in haskell is Bool
 
-isZero = undefined
+isZero 0 = True
+isZero _ = False
 
 -- Ex 8: implement using recursion a function sumTo such that
 --   sumTo n
 -- computes the sum 1+2+...+n
 
 sumTo :: Integer -> Integer
-sumTo = undefined
+sumTo 0 = 0
+sumTo n = n + sumTo (n - 1)
 
 -- Ex 9: power n k should compute n to the power k (i.e. n^k)
 -- Use recursion.
 
 power :: Integer -> Integer -> Integer
-power = undefined
+power _ 0 = 1
+power n k = n * power n (k - 1)
 
 -- Ex 10: ilog2 n should be the number of times you can halve the
 -- integer n (rounding down) before you get 1.
